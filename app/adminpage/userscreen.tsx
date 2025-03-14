356ef963d3aef4c8532f0f46676a175d1330fb3f
Isara Nakavisute <isara_nakavisute@hotmail.com>
2025-03-14 16:21:02 +0700

    admin panel

---
+++ app/adminpage/userscreen.tsx
@@ -0,0 +1,104 @@
+'use client'; 
+
+import logo from "./images/new_logo.png";
+import Image from '@/node_modules/next/image';
+import Link from '@/node_modules/next/link';
+import { FormEvent, useEffect } from 'react';
+import useRouter from '@/node_modules/next/router';
+//import useRouter from '@/node_modules/next/navigator';
+import axios from 'axios';
+import React from "react";
+import { unstable_cache } from "next/cache";
+//import MainScreen from '../mainpage/userscreen';
+
+
+
+export default function UserScreen() {
+
+  const router = useRouter;
+  const [std_json_res, set_std_json_res] = React.useState([{"Id": "value","myusername": "value","mypassword": "value","myemail": "value"}]); //React.useState(null);
+  //var std_json_res;
+
+  function autoload()
+  {
+    axios({
+     method: "POST",
+     url: "http://127.0.0.1:3005/listuser",
+     headers: {
+      "Content-Type": "application/json"
+     }
+   })
+   .then(res => {
+    set_std_json_res(res.data);
+    return res.data;
+    })
+    .catch(err => {
+   });
+ }
+
+  useEffect(() => {
+    autoload();
+  }, []);
+
+  return (
+        <div className="flex flex-col w-full h-[100vh] bg-black">                 
+         <div className="flex h-full w-full flex-row items-center justify-center bg-black">
+          <div className="flex h-[95vh] w-full bg-black flex-col justify-start items-center rounded-xl">
+            <div className="flex flex-row w-full h-[5vh] border-0 border-solid border-black items-center justify-center bg-amber-600">
+             <div className="text-3xl">Admin Page</div>
+            </div>
+            <div className="flex flex-col w-full h-[5vh] border-0 border-solid border-purple-400 items-center justify-start"> 
+             <div className="flex flex-row w-9/10 h-full border-0 border-solid border-black">
+              <div className="flex flex-row w-1/4 h-full border-1 border-solid border-black bg-amber-400 justify-center">USER ID</div>
+              <div className="flex flex-row w-1/4 h-full border-1 border-solid border-black bg-amber-400 justify-center">USERNAME</div>
+              <div className="flex flex-row w-1/4 h-full border-1 border-solid border-black bg-amber-400 justify-center">PASSWORD</div>
+              <div className="flex flex-row w-1/4 h-full border-1 border-solid border-black bg-amber-400 justify-center">EMAIL</div>
+             </div>
+            </div>
+            {/* <div className="flex flex-col w-full h-[5vh] bg-white justify-start items-center mb-10"> 
+             <div className="flex flex-row w-9/10 h-full bg-black">
+             </div>
+            </div> */}
+            <div className="flex flex-col w-9/10 h-full border-1 border-solid border-purple items-center justify-start overflow-y-auto"> 
+            {/* <div className="flex flex-col w-9/10 h-full border-1 border-solid border-yellow items-center justify-center">  */}
+             {/* <table className="border-1 border-solid border-purple w-full h-full">
+             <tr className="border-1 border-solid border-black w-full">
+                  <td className="border-1 border-solid border-black w-1/4"><div>1</div></td>
+                  <td className="border-1 border-solid border-black w-1/4"><div>1</div></td>
+                  <td className="border-1 border-solid border-black w-1/4"><div>1</div></td>
+                  <td className="border-1 border-solid border-black w-1/4"><div>1</div></td>
+             </tr> */}
+             {
+              std_json_res.map((post) => (
+                // <tr className="border-1 border-solid border-black w-full">
+                //   <td className="border-1 border-solid border-black w-1/4"><div>{post.Id}</div></td>
+                //   <td className="border-1 border-solid border-black w-1/4"><div>{post.myusername}</div></td>
+                //   <td className="border-1 border-solid border-black w-1/4"><div>{post.mypassword}</div></td>
+                //   <td className="border-1 border-solid border-black w-1/4"><div>{post.myemail}</div></td>
+                // </tr>
+              
+                  // <tr className="border-1 border-solid border-black w-full">
+                  //   <td className="border-1 border-solid border-black w-1/4"><div>{post.Id}</div></td>
+                  //   <td className="border-1 border-solid border-black w-1/4"><div>{post.myusername}</div></td>
+                  //   <td className="border-1 border-solid border-black w-1/4"><div>{post.mypassword}</div></td>
+                  //   <td className="border-1 border-solid border-black w-1/4"><div>{post.myemail}</div></td>
+                  // </tr>
+
+                  <div className="flex flex-row w-full bg-amber-100">
+                   <div className="border-1 border-solid border-black w-1/4 pl-2">{post.Id}</div>
+                   <div className="border-1 border-solid border-black w-1/4 pl-2">{post.myusername}</div>
+                   <div className="border-1 border-solid border-black w-1/4 pl-2">{post.myusername}</div>
+                   <div className="border-1 border-solid border-black w-1/4 pl-2">{post.myusername}</div>
+                  </div>
+                
+               ) 
+              )
+             }
+             {/* </table> */}
+            {/* </div> */}
+            </div>
+          </div>
+         </div>
+        </div>
+    );
+}
--- app/register/userscreen.tsx
+++ app/register/userscreen.tsx
@@ -21,46 +21,40 @@ export default function UserScreen() {
     const formData = new FormData(event.currentTarget);
     const username = formData.get('username');
     const password = formData.get('password');
+    const email = formData.get("email");
 
     axios({
      method: "POST",
-     url: "http://127.0.0.1:3005/login",
+     url: "http://127.0.0.1:3005/register",
      headers: {
       "Content-Type": "application/json"
      },
      data: {
       username: username,
-      password: password
+      password: password,
+      email: email
     },
    })
    .then(res => {
     var std_json_res = res.data;
-    //alert(std_json_res.login.result);
-    console.log(std_json_res.login.result);
-    if (std_json_res.login.result=="pass")
+    alert(std_json_res.register.result);
+    console.log(std_json_res.register.result);
+    if (std_json_res.register.result=="pass")
      {
-      if (std_json_res.login.username=="admin")
-       window.location.href = "/adminpage";
-      else
-       window.location.href = "/mainpage";
+      alert('user registered');
      }
     })
     .catch(err => {
    });
  }
 
- async function openWindow()
- {
-  window.open('/register', '_blank');
- }
-
   return (
        <form onSubmit={handleOnSubmit}>
         <div className="flex flex-col">                 
          <div className="flex h-[100vh] w-full flex-row items-center justify-center bg-black">
-          <div className="flex h-1/2 w-1/2 bg-white flex-col justify-center rounded-xl">
+          <div className="flex h-full w-1/2 bg-white flex-col justify-start rounded-xl">
             <div className="flex flex-row w-full h-[5vh] border-0 border-solid border-black items-center justify-center mb-15 mt-10">
-             <div className="text-3xl">WEBCRAWLER</div>
+             <div className="text-3xl">Please register your account</div>
             </div>
             <div className="flex flex-row w-full h-[5vh] border-0 border-solid border-black items-center justify-start mb-10">
              <div className="text-3xl h-full w-1/4 border-0 border-solid border-black flex flex-col justify-center ml-5">USERNAME</div>
@@ -70,14 +64,13 @@ export default function UserScreen() {
              <div className="text-3xl h-full w-1/4 border-0 border-solid border-black flex flex-col justify-center ml-5">PASSWORD</div>
              <div className="h-full w-3/4 border-0 border-solid border-black flex flex-row justify-start items-center"><input type="text" name="password" className="h-full w-full border border-solid border-yellow text-2xl cursor-wait mr-5"  /></div>
             </div>
+            <div className="flex flex-row w-full h-[5vh] border-0 border-solid border-black items-center justify-start mb-10">
+             <div className="text-3xl h-full w-1/4 border-0 border-solid border-black flex flex-col justify-center ml-5">EMAIL</div>
+             <div className="h-full w-3/4 border-0 border-solid border-black flex flex-row justify-start items-center"><input type="text" name="email" className="h-full w-full border border-solid border-yellow text-2xl cursor-wait mr-5"  /></div>
+            </div>
             <div className="flex flex-row w-full h-[5vh] border-0 border-solid border-black items-center justify-center mb-0">
-              <input type="submit" className="pl-2 pr-2 pt-2 pb-2 mr-5 bg-amber-200 h-full w-[20vh] rounded-lg" value="Log In" />
+              <input type="submit" className="pl-2 pr-2 pt-2 pb-2 mr-5 bg-amber-200 h-full w-[20vh] rounded-lg" value="Register" />
               <input type="reset" className="pl-2 pr-2 pt-2 pb-2 bg-amber-200 h-full w-[20vh] rounded-lg" value="Cancel" />
-            </div>
-            <div className="flex flex-row w-full h-[5vh] border-0 border-solid border-black items-start justify-center mt-10">
-            <div className="text-xs text-black">If you dont have an account, please</div><div className="text-xs text-red-300 ml-1">
-             <Link href="" onClick={openWindow}>register</Link>
-            </div>
             </div>
           </div>
          </div>
